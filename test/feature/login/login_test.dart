0a31e02e9c79a8557587a60a9ed720ad7f7e962e
Yunus Emre CAKMAZ <y.emrecakmaz@gmail.com>
2021-04-10 23:29:10 +0300

    initial commit

---
+++ test/feature/login/login_test.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_test/flutter_test.dart';
+
+main() {
+  setUp(() {
+    print("Here");
+  });
+
+  test("User Login Fail Test", () {
+    final isUserLogin = true;
+
+    expect(isUserLogin, isTrue);
+  });
+
+  group("User Login Full Test", () {
+    test("User Login Fail Test", () {
+      final isUserLogin = true;
+
+      expect(isUserLogin, isTrue);
+    });
+
+    test("User Login Fail Test", () {
+      final isUserLogin = true;
+
+      expect(isUserLogin, isTrue);
+    });
+  });
+}
